b3e1b0f03ff751f072f4a1cab8d3fcf66286b778
Davide Sofronia <davidesofronia@gmail.com>
2020-03-18 03:43:52 +0100

    Start of the project

---
+++ macchange.sh
@@ -0,0 +1,4 @@
+#dodrisomli@enayu.com
+ip link set wlp0s18u1u1 down
+macchanger -r wlp0s18u1u1
+ip link set wlp0s18u1u1 up
---
+++ prova.py
@@ -0,0 +1,41 @@
+from selenium.webdriver import Firefox
+from selenium.webdriver.firefox.options import Options
+import time
+
+'''opts = Options()
+opts.set_headless()
+assert opts.headless'''
+browser = Firefox()
+browser.get('http://detectportal.firefox.com')
+assert "Vodafone" in browser.title
+print(browser.title)
+
+consentcookies = browser.find_element_by_class_name('acceptButton')
+consentcookies.click()
+time.sleep(1)
+redirect_form = browser.find_element_by_link_text('Try 15 mins. Free trial >')
+redirect_form.click()
+
+print(browser.title)
+
+email_form = browser.find_element_by_id('email')
+email_form.send_keys('xueigtsdalvb@dropmail.me')
+time.sleep(2)
+
+passwd_form = browser.find_element_by_id('password')
+passwd_form.send_keys('vodapwd123')
+time.sleep(2)
+
+repeat_passwd_form = browser.find_element_by_id('register_repeat-password') 
+repeat_passwd_form.send_keys('vodapwd123')
+time.sleep(2)
+browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+checkbox_form = browser.find_element_by_id('register_terms')
+checkbox_form.click()
+time.sleep(2)
+submit_form = browser.find_element_by_id('signup')
+submit_form.click()
+time.sleep(2)
+
+browser.close()
